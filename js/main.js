6f9943ae6ceea4b8eb879ecf1c65e270cfdd60c6
Koichiro12 <choiruldhani889@gmail.com>
2023-02-07 14:19:26 +0700

    update

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@
 			scaleColor: false,
 			lineWidth: 4,
 			lineCap: 'butt',
-			barColor: '#FF9000',
+			barColor: 'rgba(0, 102, 255, 0.9)',
 			trackColor:	"#f5f5f5",
 			size: 160,
 			animate: 1000
